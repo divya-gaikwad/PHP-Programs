cc34911de53b44dada14775f90d2796cb1af5981
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-03-14 15:02:04 +0530

    Add files via upload

---
+++ slip4.php
@@ -0,0 +1,13 @@
+<html>
+	<body>
+	<form method="post" action="slip4(2).php">
+	eno=
+	<input type="text" name="t1">
+	ename=
+	<input type="text" name="t2">
+	add=
+	<input type="text" name="t3">
+	<input type="submit">
+	</form>
+	</body>
+</html>
