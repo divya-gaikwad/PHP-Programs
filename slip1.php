208148ce95a28ef6634775585f8dcf78a98f335a
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-03-14 15:01:02 +0530

    Add files via upload

---
+++ slip1.php
@@ -0,0 +1,13 @@
+<?php
+	
+session_start();
+	if(!isset($_SESSION["count"]))
+	{
+		$_SESSION["count"]=1;
+	}
+	else
+	{
+		$_SESSION["count"]=$_SESSION["count"]+1;
+		echo("visit count=".$_SESSION["count"]);
+	}	
+?>
