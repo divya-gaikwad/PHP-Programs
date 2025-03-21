cacfcdf192f9c2b747d7a3dd0a1da3eb1e912e10
DIVYA RAVINDRA GAIKWAD <156771617+divya-gaikwad@users.noreply.github.com>
2025-03-21 12:11:45 +0530

    Add files via upload

---
+++ catlog.php
@@ -0,0 +1,12 @@
+<?php
+header('content-type=text/xml');
+echo("<?xml version='1.0' encoding='UTF-8'?>");
+echo("<cd>");
+echo("<titl>mi..................</titl>");
+echo("<artist >t        u</artist>");
+echo("</cd>");
+
+
+
+
+?>
